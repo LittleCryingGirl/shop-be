3065956fcc8f2f0c026f44b2ddc38392552afc86
Svetlana Beliaeva <svetlana_beliaeva@epam.com>
2020-11-01 00:22:42 +0300

    Add products service

---
+++ product-info-service/handlers/getProductById.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import * as products from '../staticData/productsList.json';
+import 'source-map-support/register';
+import { headers } from "../constants";
+
+export const getProductById: APIGatewayProxyHandler = async (event, _context) => {
+  const productId = event.queryStringParameters?.productId;
+
+  if (!productId) {
+    return {
+      statusCode: 400,
+      body: "Wrong parameters",
+      headers
+    }
+  }
+
+  const product = products.find(p => p.id === productId);
+
+  if (product) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product),
+      headers
+    };
+  }
+  
+  return {
+    statusCode: 404,
+    body: 'Product not found',
+    headers
+  }
+}
